feacb3cbc13f1f45ab90dd22e2926cd5e3f19193
3nsar <84131267+3nsar@users.noreply.github.com>
2024-12-20 22:34:56 +0100

    commit

--- flexlab/src/components/Footer.tsx
+++ flexlab/src/components/Footer.tsx
@@ -2,7 +2,13 @@
 
 const Footer = () => {
   return (
-    <div className="min-h-10">
+    <div className="min-h-10 flex justify-center text-left">
+      <ul className="bg-green-500">
+        <li>Social Media</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+
       <ul className="bg-green-500">
         <li>Social Media</li>
         <li>Menu</li>
